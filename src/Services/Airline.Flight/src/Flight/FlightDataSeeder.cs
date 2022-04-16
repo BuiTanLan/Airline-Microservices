8643447ceea2d4c751203a3bcf7f1f82e0c7895a
meysamhadeli <meysamhadeli@gmail.com>
2022-04-17 02:23:26 +0430

    fix seed

--- src/Services/Airline.Flight/src/Flight/FlightDataSeeder.cs
+++ src/Services/Airline.Flight/src/Flight/FlightDataSeeder.cs
@@ -38,55 +38,55 @@ public class FlightDataSeeder : IDataSeeder
 
     private async Task SeedAirportAsync()
     {
-        if (await _airportRepository.Find(1, CancellationToken.None) == null)
+        if (await _airportRepository.Find(2399187385188352, CancellationToken.None) == null)
         {
             await _airportRepository.Add(
-                Airport.Create(1, "Lisbon International Airport", "LIS", "12988"), CancellationToken.None);
+                Airport.Create(2399187385188352, "Lisbon International Airport", "LIS", "12988"), CancellationToken.None);
 
             await _airportRepository.Add(
-                Airport.Create(2, "Tehran International Airport", "Teh", "18000"), CancellationToken.None);
+                Airport.Create(2499187385188353, "Tehran International Airport", "Teh", "18000"), CancellationToken.None);
         }
     }
 
 
     private async Task SeedFlightAsync()
     {
-        if (await _flightRepository.Find(1, CancellationToken.None) == null)
+        if (await _flightRepository.Find(7799187385188353, CancellationToken.None) == null)
         {
             await _flightRepository.Add(
-                Flights.Models.Flight.Create(1, "12BB", 1, 1, new DateTime(2022,6,1, 10, 0 , 0),
-                    new DateTime(2022,6,1, 14, 0, 0), 2, 200, new DateTime(2022,6,1),
+                Flights.Models.Flight.Create(7799187385188353, "12CC", 1399187385188352, 2399187385188352, new DateTime(2022,6,1, 10, 0 , 0),
+                    new DateTime(2022,6,1, 14, 0, 0), 2499187385188353, 200, new DateTime(2022,6,1),
                     FlightStatus.Completed, 8000), CancellationToken.None);
         }
     }
 
     private async Task SeedAircraftAsync()
     {
-        if (await _aircraftRepository.Find(1, CancellationToken.None) == null)
+        if (await _aircraftRepository.Find(1399187385188352, CancellationToken.None) == null)
         {
             await _aircraftRepository.Add(
-                Aircraft.Create(1, "Boeing 737", "B737", 2005), CancellationToken.None);
+                Aircraft.Create(1399187385188352, "Boeing 737", "B737", 2005), CancellationToken.None);
         }
     }
 
     private async Task SeedSeatAsync()
     {
-        if (await _seatRepository.Find(1, CancellationToken.None) == null)
+        if (await _seatRepository.Find(4399187385188352, CancellationToken.None) == null)
         {
             await _seatRepository.Add(
-                Seat.Create(1, "12A", SeatType.Window, SeatClass.Economy, 1), CancellationToken.None);
+                Seat.Create(4399187385188352, "12A", SeatType.Window, SeatClass.Economy, 7799187385188353), CancellationToken.None);
 
             await _seatRepository.Add(
-                Seat.Create(2, "12B", SeatType.Window, SeatClass.Economy, 1), CancellationToken.None);
+                Seat.Create(4499187385188352, "12B", SeatType.Window, SeatClass.Economy, 7799187385188353), CancellationToken.None);
 
             await _seatRepository.Add(
-                Seat.Create(3, "12C", SeatType.Middle, SeatClass.Economy, 1), CancellationToken.None);
+                Seat.Create(4599187385188352, "12C", SeatType.Middle, SeatClass.Economy, 7799187385188353), CancellationToken.None);
 
             await _seatRepository.Add(
-                Seat.Create(4, "12D", SeatType.Middle, SeatClass.Economy, 1), CancellationToken.None);
+                Seat.Create(4699187385188352, "12D", SeatType.Middle, SeatClass.Economy, 7799187385188353), CancellationToken.None);
 
             await _seatRepository.Add(
-                Seat.Create(5, "12F", SeatType.Aisle, SeatClass.Economy, 1), CancellationToken.None);
+                Seat.Create(4799187385188352, "12F", SeatType.Aisle, SeatClass.Economy, 7799187385188353), CancellationToken.None);
         }
     }
 }
